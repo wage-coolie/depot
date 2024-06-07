c9060d7541228ff0e5dda228092648c43d03126b
उत्कर्ष ॥ Utkarsh <endgithub@protonmail.com>
2024-06-07 10:13:12 +0530

    chap 12 done

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+		if defined?(Rails::Server) && Rails.env.development?
+		  require "debug/open_nonstop"
+		end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
